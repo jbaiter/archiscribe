8807956bdd8e7249eeefd936f54c10ced8c187be
Johannes Baiter <johannes.baiter@gmail.com>
2017-10-29 18:31:49 +0100

    Increase font-weight on keyboard

--- client/src/components/Keyboard.vue
+++ client/src/components/Keyboard.vue
@@ -100,6 +100,7 @@ button.keycap {
   border-radius: 4px;
   box-shadow: 0px 1px 3px 1px rgba(0, 0, 0, 0.5);
   font: 16px/40px sans-serif ;
+  font-weight: 600;
   text-align: center;
   color: #666;
 }
